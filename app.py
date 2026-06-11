adde82b9d5734e698411b327abd0070f38eb5ad0
krish <23bmiit169@gmail.com>
2026-06-11 17:57:08 +0530

    requirmentv.3

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    f"mysql+pymysql://{os.environ['MYSQLUSER']}:"
+    f"{os.environ['MYSQLPASSWORD']}@"
+    f"{os.environ['MYSQLHOST']}:"
+    f"{os.environ['MYSQLPORT']}/"
+    f"{os.environ['MYSQLDATABASE']}"
+)
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
@@ -56,5 +66,8 @@ def delete(sno):
     db.session.commit()
     return redirect("/")
 
+with app.app_context():
+    db.create_all()
+    
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
